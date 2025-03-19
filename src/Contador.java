fdade426159e5e51b0d2bd9a8aa6163728ec4ce2
Lucas Braga <lucasrbraga77@gmail.com>
2025-03-19 16:06:48 -0300

    Adicionadas alterações para Contador.java e ParametrosInvalidosException.java.

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,27 @@
-public class Main {
+import java.util.Scanner;
+
+public class Contador {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = input.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois =input.nextInt();
+        try{
+        contar(parametroUm,parametroDois);
+        }catch (ParametrosInvalidosException Exception){
+            System.out.println("ERRO: " + Exception + "\nO segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+    }
+    static void contar(int parametroUm,int parametroDois)throws ParametrosInvalidosException{
+        if(!(parametroUm < parametroDois)) {
+            int contagem = parametroUm - parametroDois;
+            for (int indice = 1; indice <= contagem; indice++) {
+                System.out.println("Imprimindo o número " + indice + ".");
+            }
+        }else{
+            throw new ParametrosInvalidosException();
+        }
     }
 }
